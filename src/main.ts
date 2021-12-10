834969e1a4ac309be3ec519ce3539ec4c0bd46db
Eric <eric.lopezs@alumnos.udg.mx>
2021-12-10 03:36:16 -0600

    [Semantical] throw the towel (temporarily)

--- src/main.ts
+++ src/main.ts
@@ -18,11 +18,18 @@ function padAndFormat(token: Token) {
 
 function main() {
   const input = `
-    int main() {
-      if(1 == 3)
-        printf("Hello, World!");
-      return 0;
-    }
+  int a;
+  int suma(int a, int b){
+  return a+b;
+  }
+  
+  int main(){
+  float a;
+  int b;
+  int c;
+  c = a+b;
+  c = suma(8,9);
+  }
     `;
 
   const { tokens } = scan(input, patterns);
--- src/parser/syntaxer.ts
+++ src/parser/syntaxer.ts
@@ -19,6 +19,7 @@ export interface SyntaxDescription {
 export interface StackItem {
   type: "token" | "state" | "expression";
   data: number | string;
+  children?: StackItem[];
 }
 
 export function parse(
@@ -29,8 +30,9 @@ export function parse(
   const stack: StackItem[] = [{ type: "state", data: 0 }];
   let tokenIndex = 0;
   let token: Token = undefined;
-
-  while ((token = tokens[tokenIndex])) {
+  let tree: StackItem;
+  while (true) {
+    token = tokens[tokenIndex];
     const tokenId = token.code;
     let currentState = <number>last(stack).data;
     let action = grammarSyntax.transitionAt(currentState, tokenId);
@@ -45,7 +47,7 @@ export function parse(
     } else if (action.type == "reduce") {
       const reducedRule = grammarSyntax.getRuleAtIndex(action.code);
       const popCount = reducedRule.tokenCount * 2;
-      stack.splice(stack.length - popCount, popCount);
+      const poppedElements = stack.splice(stack.length - popCount, popCount);
       currentState = <number>last(stack).data;
       action = grammarSyntax.transitionAt(currentState, reducedRule.code);
       debug(stack, token, grammarSyntax, reducedRule.code);
@@ -53,11 +55,20 @@ export function parse(
       if (action.type == "finish") return {};
 
       const newState = action.code;
-      stack.push({ type: "expression", data: reducedRule.name });
+      //https://github.com/lmenezes/json-tree
+      tree = {
+        type: "expression",
+        data: reducedRule.name,
+        children: poppedElements,
+      };
+      stack.push({
+        type: "expression",
+        data: reducedRule.name,
+      });
       stack.push({ type: "state", data: newState });
     } else {
       throw new Error("Invalid action");
     }
   }
-  return true;
+  return tree;
 }
